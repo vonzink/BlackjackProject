f943130e199a8b894367c24a1a343b2b0a93ad11
Vonzink <vonzink@gmail.com>
2025-03-29 16:42:38 -0600

    have the pieced, workin on logic for main

--- src/com/skilldistillery/cards/blackjack/BlackjackApplication.java
+++ src/com/skilldistillery/cards/blackjack/BlackjackApplication.java
@@ -3,62 +3,72 @@ package com.skilldistillery.cards.blackjack;
 import java.util.Scanner;
 
 public class BlackjackApplication {
-	
-	//NO MORE FIELDS
-	//NO DECK TYPE VARIABLES
-	
-	private Scanner sc; 
-	private Dealer dealer; 
-	private Player player; 
-	
+
+	// NO MORE FIELDS
+	// NO DECK TYPE VARIABLES
+
+	private Scanner sc;
+	private Dealer dealer;
+	private Player player;
+
 	public static void main(String[] args) {
 		new BlackjackApplication().launch();
-		
+
 	}
+
 	public BlackjackApplication() {
-			this.dealer = new Dealer();
-			this.player = new Player(); 
+		this.dealer = new Dealer();
+		this.player = new Player();
 	}
 
 	public void launch() {
-		this.sc = new Scanner(System.in) ;
+		this.sc = new Scanner(System.in);
 		System.out.println("Welcome to Blackjack!");
 		System.out.println();
 		do {
-			playRound(); 
-			System.out.println("play another round?");
-			String answer = sc.nextLine(); 
-			if (answer.equalsIgnoreCase("n")); 
-			playAgain(); 
-				
-		} while (playAgain()); 
-	  System.out.println(", goodbye!");
+			playRound();
+
+		} while (playAgain());
+		System.out.println(", goodbye!");
 	}
+
 	public void playRound() {
 		dealer.reset();
 		player.resetHand();
 		dealer.beginPlay(player);
-		playersTurn(); 
 		System.out.print("Dealers Hand: ");
-			dealer.displayHand(false);
-			System.out.println();
-		System.out.println("Players Hand ");
-			player.getHandValue(); 
+		dealer.displayHand(false);
+		System.out.println();
+		player.displayHand();
+		System.out.println(player.getHandValue());
+		playersTurn();
 	}
+
 	private void playersTurn() {
-		this.sc = new Scanner(System.in) ;
-		player.getHandValue(); 
-		
+		System.out.println("Do you want to hit or stay?");
+		String hit = sc.nextLine();
+		if (hit.equalsIgnoreCase("hit")) {
+			player.hit(dealer.dealCard());
+			player.displayHand();
+			if (player.getHandValue() > 21) {
+				System.out.println("Bust!");
+			} else {
+				this.playersTurn();
+			}
+		}
 	}
+
 	public void dealersTurn() {
-		
-		
+		dealer.getHandValue();
+	}
+
 	public boolean playAgain() {
-		boolean playAgain = false; 
+		boolean playAgain = true;
 		System.out.println("play another round?");
-		String answer = sc.nextLine(); 
-		if (answer.equalsIgnoreCase("y")); 
-		return playAgain = true; 
+		String answer = sc.nextLine();
+		if (answer.equalsIgnoreCase("y"))
+			;
+		return playAgain;
 	}
 
 }
